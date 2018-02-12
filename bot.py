2dce2879190763048bc4712c25f5578bf3a4c16a
mateuszdrwal <drwal.mateusz@gmail.com>
2018-02-12 15:22:19 +0100

    rewrote most if it moving to pygsheets, adding marking requests as planned, rejected and implemented and adding developer comments

--- bot.py
+++ bot.py
@@ -1,68 +1,130 @@
-import discord, asyncio, gspread, datetime, re, time
+import discord, asyncio, pygsheets, datetime, re, time
 from oauth2client.service_account import ServiceAccountCredentials
 
 client = discord.Client()
 
 reqVals = []
 featVals = []
-otherVals = []
+rejVals = []
+doneVals = []
+planVals = []
+
+gs = pygsheets.authorize(service_file="key.json")
+
+ss = gs.open("Echo Arena Feature Requests")
+allS = ss.worksheet_by_title("All")
+openS  = ss.worksheet_by_title("Open")
+rejS  = ss.worksheet_by_title("Rejected")
+doneS  = ss.worksheet_by_title("Implemented")
+planS  = ss.worksheet_by_title("Planned")
+stats = ss.worksheet_by_title("Stats")
 
 pattern = re.compile(r"\*?\*?What kind of submission is this\?[\*,:, ]*(.*?) ?\n\n?\*?\*?Title[\*,:, ]*(.*?) ?\n\*?\*?Category[\*,:, ]*(.*?) ?\n\*?\*?Description[\*,:, ]*([\s\S]*)")
+commentpattern = re.compile("(\d{18}) *?([\s\S]*)")
+
+async def perm(reaction):
+    global guild
+    async for user in reaction.users():
+        try:
+            member = guild.get_member(user.id)
+            if discord.utils.find(lambda r: r.name == "Moderator" or r.name == "Developer" or member.name == "mateuszdrwal", member.roles) != None:
+                return True                             
+        except:
+            return False
 
 def updateSheet():
-    global sheet, reqVals, featVals, otherVals
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('./key.json', ['https://spreadsheets.google.com/feeds'])
-    gs = gspread.authorize(credentials)
+    global reqVals, featVals, rejVals, doneVals, planVals
+    
 
-    ss = gs.open("Echo Arena Feature Requests")
-    s1 = ss.worksheet("Feature Request")
-    s2 = ss.worksheet("Improving an Existing Feature")
-    s3 = ss.worksheet("Other")
-    s4 = ss.worksheet("All")
+    for sheet, entries in [(openS, featVals+reqVals),(doneS, doneVals),(planS,planVals),(allS, reqVals+featVals+rejVals+doneVals+planVals)]:
+        #if len(entries) == 0:
+        #continue
+        
+        newVals = []
+        
+        for entry in entries:
+            if entry["status"] == 0:
+                status = "Open"
+            if entry["status"] == 1:
+                status = "Planned"
+            if entry["status"] == 2:
+                status = "Rejected"
+            if entry["status"] == 3:
+                status = "Implemented"
 
-    for sheet, newVals in [(s1, reqVals),(s2, featVals), (s3, otherVals), (s4, reqVals+featVals+otherVals)]:
-        if len(newVals) == 0:
-            continue
-        newVals.sort(key=lambda x: int(x[6]))
+            newVals.append([entry["time"],
+                            entry["author"],
+                            entry["type"],
+                            entry["title"],
+                            entry["category"],
+                            entry["description"],
+                            entry["points"],
+                            entry["upvotes"],
+                            entry["downvotes"],
+                            status,
+                            "\n\n".join(entry["devresp"]),
+                            entry["mid"],
+                            entry["aid"]
+                            ])
+        
+        newVals.sort(key=lambda r: int(r[6]))
         newVals.reverse()
-        cells = sheet.range(2,1,len(newVals)+1,11)
-        for cell in cells:
-            cell.value = newVals[cell.row-2][cell.col-1]
+        newVals.insert(0, ["Time Submitted","Member","Suggestion Type","Title","Category","Description","Points","Upvotes","Downvotes","Status","Developer Response","Message ID","Member ID"])
 
-        sheet.update_cells(cells)                
+        cells = []
+        for i, row in enumerate(newVals):
+            for j, val in enumerate(row):
+                cells.append(pygsheets.Cell((i+1,j+1),str(val)))
+
+        sheet.clear((len(newVals)+1,1))
+        sheet.update_cells(cell_list=cells)
+
+    cells = []
+    cells.append(pygsheets.Cell("B8"))
+    cells[-1].value = "Last updated at: %s"%datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC")
+    stats.update_cells(cell_list=cells)
     
     reqVals = []
     featVals = []
-    otherVals = []
+    rejVals = []
+    doneVals = []
+    planVals = []
 
 async def loop():
-    global requestChannel, reqVals, featVals, otherVals, guild
+    global requestChannel, reqVals, featVals, rejVals, doneVals, planVals, guild
     await client.wait_until_ready()
     await asyncio.sleep(1)
     while True:
         try:
             start = time.time()
+
             
-            async for message in requestChannel.history(limit=1000,reverse=True):
+            async for message in requestChannel.history(limit=None, reverse=True):
                 
                 results = re.findall(pattern, message.content)
+                comment = re.findall(commentpattern, message.content)
+                if comment != [] and discord.utils.find(lambda r: r.name == "Moderator" or r.name == "Developer" or message.author.name == "mateuszdrwal", message.author.roles) != None:
+                    comment = comment[0]
+                    id = int(comment[0])
+                    for array in [reqVals, featVals, rejVals, doneVals]:
+                        msg = discord.utils.find(lambda r: r["mid"] == id, array)
+                        if msg != None:
+                            msg["devresp"].append(comment[1])
+                            await message.add_reaction(u"\N{WHITE HEAVY CHECK MARK}")
+                            break
+                    continue
+                    
                 if results == [] or message.id == 403337281068466197:
                     continue
 
                 upvotes = 0
                 downvotes = 0
+                status = 0 #0: open, 1: in-progress, 2: rejected, 3: resolved
                 cont = False
                 for reaction in message.reactions:
                     if reaction.emoji == "\u26d4":
-                        async for user in reaction.users():
-                            try:
-                                member = guild.get_member(user.id)
-                                if discord.utils.find(lambda r: r.name == "Moderator" or r.name == "Developer" or member.name == "mateuszdrwal", member.roles) != None:
-                                    cont = True
-                                    break                                
-                            except:
-                                pass
-                        if cont:
+                        if await perm(reaction):
+                            cont = True
                             break
                     elif reaction.emoji == "\U0001f44d":
                         upvotes = reaction.count
@@ -72,29 +134,46 @@ async def loop():
                     elif reaction.emoji == "\U0001f44e":
                         downvotes = reaction.count
 
+                    elif reaction.emoji == "\U0001f5d3":
+                        if await perm(reaction) and status < 1:
+                            status = 1
+                    elif reaction.emoji == "\u274c":
+                        if await perm(reaction) and status < 2:
+                            status = 2
+                    elif reaction.emoji == "\u2705":
+                        if await perm(reaction) and status < 3:
+                            status = 3
+                            
                 if cont:
                     continue
 
-                entry = [message.created_at.strftime("%Y-%m-%d %H:%M:%S UTC"),
-                        "%s#%s"%(message.author.name, message.author.discriminator),
-                        results[0][0],
-                        results[0][1],
-                        results[0][2],
-                        results[0][3],
-                        str(upvotes-downvotes),
-                        str(upvotes),
-                        str(downvotes),
-                        message.id,
-                        message.author.id
-                        ]
+                entry = {"time": message.created_at.strftime("%Y-%m-%d %H:%M:%S UTC"),
+                        "author": "%s#%s"%(message.author.name, message.author.discriminator),
+                        "type": results[0][0],
+                        "title": results[0][1],
+                        "category": results[0][2],
+                        "description": results[0][3],
+                        "points": str(upvotes-downvotes),
+                        "upvotes": str(upvotes),
+                        "downvotes": str(downvotes),
+                        "status": status,
+                        "devresp": [],
+                        "mid": message.id,
+                        "aid": message.author.id
+                        }
 
                 sugType = results[0][0].lower()
-                if "feature" in sugType and ("request" in sugType or "new" in sugType):
+
+                if status == 1:
+                    planVals.append(entry)
+                elif status == 2:
+                    rejVals.append(entry)
+                elif status == 3:
+                    doneVals.append(entry)
+                elif "feature" in sugType and ("request" in sugType or "new" in sugType):
                     reqVals.append(entry)
                 elif "improv" in sugType and "existing" in sugType and "feature" in sugType:
                     featVals.append(entry)
-                else:
-                    otherVals.append(entry)
 
             updateSheet()
             end = time.time()
@@ -103,6 +182,15 @@ async def loop():
 
         except Exception as error:
             print(error)
+            gs = pygsheets.authorize(service_file="key.json")
+
+            ss = gs.open("Echo Arena Feature Requests")
+            allS = ss.worksheet_by_title("All")
+            openS  = ss.worksheet_by_title("Open")
+            rejS  = ss.worksheet_by_title("Rejected")
+            doneS  = ss.worksheet_by_title("Implemented")
+            planS  = ss.worksheet_by_title("Planned")
+            stats = ss.worksheet_by_title("Stats")
         
 @client.event
 async def on_ready():
@@ -112,6 +200,7 @@ async def on_ready():
     print(client.user.id)
 
     requestChannel = client.get_channel(403335187062194188)
+    matpmChannel = client.get_channel(412554371923050496)
     guild = client.get_guild(326412222119149578)
 
 client.loop.create_task(loop())
